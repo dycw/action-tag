8d95863b5e2c786b0b6c77ec582b3a0fe3c8581d
Derek Wan <d.wan@icloud.com>
2025-12-15 05:22:00 +0900

    Move `user.name/email` into code (#18)

--- script.py
+++ script.py
@@ -18,13 +18,15 @@ from click import command
 from typed_settings import click_options, option, settings
 from utilities.click import CONTEXT_SETTINGS_HELP_OPTION_NAMES
 from utilities.logging import basic_config
-from utilities.version import parse_version
+from utilities.version import Version, parse_version
 
 _LOGGER = getLogger(__name__)
 
 
 @settings
 class Settings:
+    user_name: str = "github-actions-bot"
+    user_email: str = "noreply@github.com"
     dry_run: bool = option(default=False, help="Dry run the CLI")
 
 
@@ -34,14 +36,26 @@ def main(settings: Settings, /) -> None:
     if settings.dry_run:
         _LOGGER.info("Dry run; exiting...")
         return
-    cmds = ["bump-my-version", "show", "current_version"]
-    _LOGGER.info("Running '%s'...", " ".join(cmds))
-    version = parse_version(check_output(cmds, text=True).rstrip("\n"))
+    _config(settings.user_name, settings.user_email)
+    version = _get_version()
     _tag(str(version))
     _tag(f"{version.major}.{version.minor}")
     _tag(str(version.major))
     _tag("latest")
     _LOGGER.info("Finished")
+
+
+def _config(name: str, email: str, /) -> None:
+    cmds1 = ["git", "config", "user.name", name]
+    _LOGGER.info("Running '%s'...", " ".join(cmds1))
+    cmds2 = ["git", "config", "user.email", email]
+    _LOGGER.info("Running '%s'...", " ".join(cmds2))
+
+
+def _get_version() -> Version:
+    cmds = ["bump-my-version", "show", "current_version"]
+    _LOGGER.info("Running '%s'...", " ".join(cmds))
+    return parse_version(check_output(cmds, text=True).rstrip("\n"))
 
 
 def _tag(version: str, /) -> None:
