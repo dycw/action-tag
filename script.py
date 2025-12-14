ee187627618a6e9e11860eba8c2f1d9684abb6d1
Derek Wan <d.wan@icloud.com>
2025-12-15 04:07:49 +0900

    2025-12-15 04:07:49 (Mon)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -11,11 +11,13 @@
 from __future__ import annotations
 
 from logging import getLogger
+from subprocess import check_call, check_output
 
 from click import command
 from typed_settings import click_options, option, settings
 from utilities.click import CONTEXT_SETTINGS_HELP_OPTION_NAMES
 from utilities.logging import basic_config
+from utilities.version import parse_version
 
 _LOGGER = getLogger(__name__)
 
@@ -31,7 +33,17 @@ def main(settings: Settings, /) -> None:
     if settings.dry_run:
         _LOGGER.info("Dry run; exiting...")
         return
-    _LOGGER.info("Running...")
+    cmds1 = ["bump-my-version", "show", "current_version"]
+    _LOGGER.info("Running '%s'...", " ".join(cmds1))
+    version = parse_version(check_output(cmds1, text=True).rstrip("\n"))
+    _LOGGER.info("Current version is %s", version)
+    cmds2 = ["git", "tag", "-a", str(version), "HEAD", "-m", str(version)]
+    _LOGGER.info("Running '%s'...", " ".join(cmds2))
+    _ = check_call(cmds2)
+    cmds3 = ["git", "push", "--tags", "--force", "--set-upstream", "orogin"]
+    _LOGGER.info("Running '%s'...", " ".join(cmds3))
+    _ = check_call(cmds3)
+    _LOGGER.info("Finished")
 
 
 if __name__ == "__main__":
